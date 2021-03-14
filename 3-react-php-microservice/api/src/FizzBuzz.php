ee30f2f34d8293a097a80c0e1fe9db9ae57b2671
arbdt <67365149+arbdt@users.noreply.github.com>
2021-03-14 13:47:44 +1030

    added basic logic to FizzBuzz.php for deciding output that matches input

--- 3-react-php-microservice/api/src/FizzBuzz.php
+++ 3-react-php-microservice/api/src/FizzBuzz.php
@@ -9,6 +9,32 @@ class FizzBuzz
 {
     public function calculate(int $start, int $end): array
     {
-        throw new LogicException('TODO: Implement FizzBuzz');
+        //throw new LogicException('TODO: Implement FizzBuzz');
+        // array to store result
+        $resultArray = array();
+
+        // iterate through given range
+        for ($counter = $start; $counter <= $end; $counter++){
+            // check for fizz (divisible by 3), buzz (divisible by 5), both, or nothing (not divisible by either)
+            // both conditions met
+            if ($counter % 3 == 0 && $counter % 5 == 0) {
+                $resultArray[] = "FizzBuzz";
+            }
+            // divisible by 3 only
+            else if ($counter % 3 == 0 && $counter % 5 != 0) {
+                $resultArray[] = "Fizz";
+            }
+            // divisible by 5 only
+            else if ($counter % 3 != 0 && $counter % 5 == 0) {
+                $resultArray[] = "Buzz";
+            }
+            // divisible by neither
+            else if ($counter % 3 != 0 && $counter % 5 != 0) {
+                $resultArray[] = "-";
+            }
+        }
+
+        // return result
+        return $resultArray;
     }
 }
