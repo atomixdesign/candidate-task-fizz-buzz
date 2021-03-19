67be3e67bbc059a1294d8c05abbf37bd4e9f5704
arbdt <67365149+arbdt@users.noreply.github.com>
2021-03-19 10:41:06 +1030

    added header access control so that front end port could access back end port

--- 3-react-php-microservice/api/src/Controllers/ApiController.php
+++ 3-react-php-microservice/api/src/Controllers/ApiController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Controllers;
 
 
@@ -18,7 +17,11 @@ class ApiController
 
     public function handle(): JsonResponse
     {
-        // TODO
+        // header cors stuff for the purpose of localhost testing this exercise
+        header("Access-Control-Allow-Origin: http://localhost:3000");
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        
         // get data from http request
         $postRequest = file_get_contents('php://input');
         if (isset($postRequest)){
