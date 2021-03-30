166efcb3587c9821a4745d95721802ed59b73c90
Revathi <revathiscicom@gmail.com>
2021-03-30 19:58:02 +1030

    Added validation and formatted code

--- 3-react-go-microservice/public/src/FizzBuzz.tsx
+++ 3-react-go-microservice/public/src/FizzBuzz.tsx
@@ -1,107 +1,155 @@
-import React,{ Component } from 'react'
+import React, { Component } from "react";
 import {
   Button,
   Form,
   Input,
   Header,
   List,
-  Container
-} from 'semantic-ui-react'
+  Container,
+  Message,
+} from "semantic-ui-react";
 
 class FizzBuzz extends Component<any, any> {
   constructor(props: any) {
-    super(props)
-    this.state = { start: '', end: '', result: [], isError: false, errors: null }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    super(props);
+    this.state = {
+      start: "",
+      end: "",
+      result: [],
+      isError: false,
+      startError: false,
+      endError: false,
+      numberError: false,
+      formError: false,
+      errors: null,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   // Form submit
   handleSubmit(event: any) {
-    event.preventDefault()
-    fetch('/api/fizzBuzz', {
-        method: 'POST',
-        //convert the state to JSON and send as the POST request
-       body: JSON.stringify(this.state)
-     }).then(response => response.json())
-     .then(data =>
-      this.setState({
-        result: data.message,
-        isError: false,
-      })
-    )
-    // Catch any errors we hit and update the app
-    .catch(error => this.setState({ error, isError: true }));
+    event.preventDefault();
+    let error = false;
+
+    //validation
+    if (this.state.start === "") {
+      this.setState({ startError: true });
+      error = true;
+    } else {
+      this.setState({ startError: false });
+    }
+
+    if (this.state.end === "") {
+      this.setState({ endError: true });
+      error = true;
+    } else {
+      this.setState({ endError: false });
+    }
+
+    if (!this.state.endError && this.state.start > this.state.end) {
+      this.setState({ numberError: true });
+      error = true;
+    } else {
+      this.setState({ numberError: false });
+    }
+
+    if (error) {
+      this.setState({ formError: true });
+      return;
+    }
+    this.setState({ formError: false });
+
+    fetch("/api/fizzBuzz", {
+      method: "POST",
+      //convert the state to JSON and send as the POST request
+      body: JSON.stringify(this.state),
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          result: data.message,
+          isError: false,
+        })
+      )
+      // Catch any errors we hit and update the app
+      .catch((error) => this.setState({ error, isError: true }));
   }
 
   // store all the values of the input field in react state single method handle
   // input changes of all the input field using ES6
   handleChange(event: any) {
     this.setState({
-      [event.target.name] : event.target.value
-    })
+      [event.target.name]: event.target.value,
+    });
   }
 
   // Returns controlled form values of the input field not stored in DOM values are exist in react component itself as state
   render() {
-    const { isError, result, error } = this.state;
-    return(
+    const { isError, result } = this.state;
+    return (
       <Container>
-      <Form onSubmit={this.handleSubmit}>
-      <Header as='h1' textAlign='center'>FizzBuzz Calculation</Header>
-        <Form.Group widths='equal'>
-         <Form.Field
-           control={Input}
-           name ='start'
-           placeholder='Start'
-           label='Start'
-           value = {this.state.start}
-           onChange={this.handleChange}
-         />
-         <Form.Field
-           control={Input}
-           label='End'
-           name ='end'
-           placeholder='End'
-           value = {this.state.end}
-           onChange={this.handleChange}
-         />
-       <Form.Field primary control={Button}  label='&nbsp;' >Calculate</Form.Field>
-       </Form.Group>
-       <hr/>
-       <List>
-           {!isError ? Object.keys(result).map((item: any) => { return <List.Item key={item}> {item}. {result[item]}</List.Item>}) : <></> }
-       </List>
-       </Form>
-       </Container>
-    )
+        <Form onSubmit={this.handleSubmit} error={this.state.formError}>
+          {this.state.formError ? (
+            <Message
+              error
+              header="FizzBuzz calculation Failed"
+              content="Please check the errors in the form"
+            />
+          ) : null}
+          <Header as="h1" textAlign="center">
+            FizzBuzz Calculation
+          </Header>
+          <Form.Group widths="equal">
+            <Form.Field
+              control={Input}
+              name="start"
+              placeholder="Start"
+              label="Start"
+              value={this.state.start}
+              onChange={this.handleChange}
+              error={this.state.startError}
+            />
+            <Form.Field
+              control={Input}
+              label="End"
+              name="end"
+              placeholder="End"
+              value={this.state.end}
+              onChange={this.handleChange}
+              error={this.state.endError || this.state.numberError}
+            />
+            {this.state.numberError ? (
+              <Message
+                error
+                content="End value should be greater than Start value"
+                pointer="below"
+              />
+            ) : null}
+
+            <Form.Field primary control={Button} label="&nbsp;">
+              Calculate
+            </Form.Field>
+          </Form.Group>
+          <hr />
+          <List>
+            {!isError ? (
+              Object.keys(result).map((item: any) => {
+                return (
+                  <List.Item key={item}>
+                    {" "}
+                    {item}. {result[item]}
+                  </List.Item>
+                );
+              })
+            ) : (
+              <></>
+            )}
+          </List>
+        </Form>
+      </Container>
+    );
   }
 }
 
 export default FizzBuzz;
-
-<form className="ui form placeholder segment">
-<header className="ui header centered">FizzBuzz Calculation</header>
-<div className="inline fields">
-<div className="field">
-<label>Start</label>
-<div className="ui input">
-<input type="text" placeholder="Start"/>
-</div>
-</div>
-<div className="field">
-<label>End</label>
-<div className="ui input">
-<input type="text" placeholder="End"/>
-</div>
-</div>
-<div className="field">
-<label>&nbsp;</label>
-<button className="ui primary button">Calculate</button>
-</div>
-</div>
-<div role="list" className="ui list">
-  <div role="listitem" className="item">
-  </div>
-</div>
-</form>
