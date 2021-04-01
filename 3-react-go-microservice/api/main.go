66a29ae4b01c4503bb3d8e15692bdbf5a5a3df0a
Revathi <revathiscicom@gmail.com>
2021-04-01 16:07:49 +1030

    Formatted code

--- 3-react-go-microservice/api/main.go
+++ 3-react-go-microservice/api/main.go
@@ -1,70 +1,74 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/contrib/static"
-  "github.com/gin-gonic/gin"
-  "encoding/json"
+    "net/http"
+    "github.com/gin-gonic/contrib/static"
+    "github.com/gin-gonic/gin"
+    "encoding/json"
 )
 
 type Input struct {
-  Start int `json:"start"`
-  End int `json:"end"`
+    Start int `json:"start"`
+    End int `json:"end"`
 }
 
 func main() {
-  // Set the router as the default one with Gin
-  router := gin.Default()
+    // Set the router as the default one with Gin
+    router: = gin.Default()
 
-  //frontend static files
-  router.Use(static.Serve("/", static.LocalFile("../../public", true)))
+    //frontend static files
+    router.Use(static.Serve("/", static.LocalFile("../../public", true)))
 
-  // Setup route group for the API
-  api := router.Group("/api")
-  {
-    api.GET("/", func(c *gin.Context) {
-      c.JSON(http.StatusOK, gin.H {
-        "message": "pong",
-      })
-    })
-  }
+    // Setup route group for the API
+    api: = router.Group("/api") {
+        api.GET("/", func(c * gin.Context) {
+            c.JSON(http.StatusOK, gin.H {
+                "message": "pong",
+            })
+        })
+    }
 
- //call API post function
-  api.POST("/fizzBuzz", FizzBuzzCalc)
+    //call API post function
+    api.POST("/fizzBuzz", FizzBuzzCalc)
 
-  // Start and run the server
-  router.Run(":5000")
+    // Start and run the server
+    router.Run(":5000")
 }
 
 //FizzBuzz calculation
-func FizzBuzzCalc(c *gin.Context) {
-    errormessage := "Input is required"
-    jsonData, err := c.GetRawData()
+func FizzBuzzCalc(c * gin.Context) {
+    errormessage: = "Input is required"
+    jsonData,
+    err: = c.GetRawData()
     if err != nil {
-      c.JSON(http.StatusBadRequest, gin.H{"error": errormessage})
+        c.JSON(http.StatusBadRequest, gin.H {
+            "error": errormessage
+        })
     }
-    input  := Input{}
-    json.Unmarshal(jsonData, &input)
+    input: = Input {}
+    json.Unmarshal(jsonData, & input)
 
-    start := input.Start;
-    end := input.End;
-    fizz := "Fizz"
-    buzz := "Buzz"
-    defValue := "-"
+    start: = input.Start;
+    end: = input.End;
+    fizz: = "Fizz"
+    buzz: = "Buzz"
+    defValue: = "-"
 
-    var output map[int]string
-    output = make(map[int]string)
-    for i := start; i <= end; i++ {
-    	if i % 3 == 0 && i % 5 == 0 {
-        output[int(i)] = fizz + buzz
-    	} else if i % 3 == 0 {
-        output[int(i)] = fizz
-    	} else if i % 5 == 0 {
-        output[int(i)] = buzz
-    	} else {
-    		output[int(i)] = defValue
-    	}
+    var output map[int] string
+    output = make(map[int] string)
+    for i: = start;i <= end;i++{
+        if i % 3 == 0 && i % 5 == 0 {
+            output[int(i)] = fizz + buzz
+        } else if i % 3 == 0 {
+            output[int(i)] = fizz
+        } else if i % 5 == 0 {
+            output[int(i)] = buzz
+        } else {
+            output[int(i)] = defValue
+        }
     }
 
-    c.JSON(http.StatusOK, gin.H{"message": output})
+    c.JSON(http.StatusOK, gin.H {
+        "message": output
+    })
 }
