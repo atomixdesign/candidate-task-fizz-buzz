4020e15377937cb4a6cbcb58f8709a061a9dbae2
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-06-21 14:56:03 +0930

    feat: added php fizzbuzz functionality

--- 3-react-php-microservice/api/src/FizzBuzz.php
+++ 3-react-php-microservice/api/src/FizzBuzz.php
@@ -9,6 +9,19 @@ class FizzBuzz
 {
     public function calculate(int $start, int $end): array
     {
-        throw new LogicException('TODO: Implement FizzBuzz');
+        for ($i = $start; $i < $end; $i++) {
+            if ($i % 3 === 0 && $i % 5 === 0) {
+                return "FizzBuzz";
+            }
+            else if ($i % 3 === 0) {
+                return "Fizz";
+            }
+            else if ($i % 3 === 0) {
+                return "Buzz";
+            }
+            else {
+                return '';
+            }
+        }
     }
 }
