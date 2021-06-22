51b1950bc557ba4f4e0c6343e2e75f954a35c8ca
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-06-22 17:44:06 +0930

    config: adding controller logic

--- 3-react-php-microservice/api/src/Controllers/ApiController.php
+++ 3-react-php-microservice/api/src/Controllers/ApiController.php
@@ -18,10 +18,21 @@ class ApiController
 
     public function handle(): JsonResponse
     {
-       $request = file_get_contents('php://input');
-       $jsonRequest = json_decode($jsonRequest);
-       $start = $jsonRequest->start;
-       $end = $jsonRequest->end;
+        // Set CORS policy as the app is decoupled
+        header("Access-Control-Allow-Origin: http://localhost:3000");
+        header('Access-Control-Allow-Methods: GET');
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+        // Get data from route
+        $request = file_get_contents('php://input');
+        $jsonRequest = json_decode($request);
+
+        // Deconstructing request
+        $start = $jsonRequest->start;
+        $end = $jsonRequest->end;
+
+        // Returning FizzBuzz
+        $returnObj = $this->fizzBuzz->calculate($start, $end);
+        return new JsonResponse($returnObj);
     }
 }
