825d2f12a50a2e0b443e79c468cb7fbc7d82d441
Yingyao Lu <emilylu123@gmail.com>
2021-09-07 23:10:48 +0930

    complete fizzBuzz function

--- 2-react/src/fizzBuzz.ts
+++ 2-react/src/fizzBuzz.ts
@@ -1,10 +1,31 @@
-export type FizzBuzzOutput = 'Fizz' | 'Buzz' | 'FizzBuzz' | ''
+export type FizzBuzzOutput = "Fizz" | "Buzz" | "FizzBuzz" | "";
 
 export type FizzBuzzResult = {
-  input: number
-  output: FizzBuzzOutput
-}
+  input: number;
+  output: FizzBuzzOutput;
+};
 
+// Implement FizzBuzz function
 export const fizzBuzz = (start: number, end: number): FizzBuzzResult[] => {
-  throw 'TODO: Implement FizzBuzz'
-}
+  const results: FizzBuzzResult[] = [];
+  const fizzBuzzFn = () => {
+    for (let num = start; num <= end; num++) {
+      const result: FizzBuzzResult = {
+        input: num,
+        output: "",
+      };
+
+      if (num % 3 === 0) {
+        result.output += "Fizz";
+      }
+      if (num % 5 === 0) {
+        result.output += "Buzz";
+      }
+      results.push(result);
+    }
+  };
+
+  fizzBuzzFn();
+
+  return results;
+};
