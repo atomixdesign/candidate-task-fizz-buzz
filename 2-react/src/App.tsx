d946f9f346a7466394113c276d9438871a2d5ff2
Yingyao Lu <emilylu123@gmail.com>
2021-09-07 23:24:17 +0930

    add fizzbuzzFn to setResult; render results

--- 2-react/src/App.tsx
+++ 2-react/src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
-import { fizzBuzz } from "./fizzBuzz";
+import { fizzBuzz, FizzBuzzResult } from "./fizzBuzz";
 
 function App() {
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
+  const [result, setResult] = useState<FizzBuzzResult[]>();
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
     const res = fizzBuzz(Number(start), Number(end));
-    console.log(res);
+    setResult(res);
   };
 
   const handleChange = (event: any) => {
@@ -21,13 +22,33 @@ function App() {
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label htmlFor="start">Start</label>
-        <input type="text" name="start" value={start}></input>
+        <input
+          type="text"
+          name="start"
+          value={start}
+          onChange={handleChange}
+        ></input>
         <label htmlFor="end">End</label>
-        <input type="text" name="end" value={end}></input>
+        <input
+          type="text"
+          name="end"
+          value={end}
+          onChange={handleChange}
+        ></input>
         <input type="submit" value="Calculate" className="submit-btn" />
       </form>
       <hr />
       <div>
+        {result
+          ? result.map((item, index) => {
+              return (
+                <p className="output" key={index}>
+                  {`${item.input}.`}
+                  {item.output ? item.output : "-"}
+                </p>
+              );
+            })
+          : null}
         <a
           className="App-link"
           href="https://reactjs.org"
